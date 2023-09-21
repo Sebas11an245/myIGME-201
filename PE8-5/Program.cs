ab80424c108a6a3c3e62e6acf7f070b34d09e572
Sebastian Arroyo <sebkiller245@gmail.com>
2023-09-20 22:15:15 -0400

    PE8 question 5

---
+++ PE8-5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_5
+{
+    /*
+     * Author: Sebastian Arroyo
+     * Purpose: Calculate z with a range of x and y
+     */
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the ranges and increments for x and y
+            double minX = -1.0;
+            double maxX = 1.0;
+            double minY = 1.0;
+            double maxY = 4.0;
+            double increment = 0.1;
+
+            // Calculate the numbers in the range
+            int xRange = (int)((maxX - minX) / increment) + 1;
+            int yRange = (int)((maxY - minY) / increment) + 1;
+
+            // Create a 3-dimensional array to store x, y, and z values
+            double[,,] values = new double[xRange, yRange, 3];
+
+            // Calculate and store the values of x, y, and z
+            for (int i = 0; i < xRange; i++)
+            {
+                for (int j = 0; j < yRange; j++)
+                {
+                    double x = minX + i * increment;
+                    double y = minY + j * increment;
+                    double z = (3 * Math.Pow(y, 2)) + (2 * x) - 1;
+
+                    values[i, j, 0] = x;
+                    values[i, j, 1] = y;
+                    values[i, j, 2] = z;
+                }
+            }
+
+            // Display the calculated values
+            for (int i = 0; i < xRange; i++)
+            {
+                for (int j = 0; j < yRange; j++)
+                {
+                    double x = values[i, j, 0];
+                    double y = values[i, j, 1];
+                    double z = values[i, j, 2];
+
+                    Console.WriteLine($"x = {x}, y = {y}, z = {z}");
+                }
+            }
+        }
+    }
+}

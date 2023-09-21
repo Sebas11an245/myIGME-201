45a2059fffb9834a5570e6aad1db49632695cb05
Sebastian Arroyo <sebkiller245@gmail.com>
2023-09-20 22:36:53 -0400

    PE8 question 7

---
+++ PE8-7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_7
+{
+    /*
+     * Author: Sebastian Arroyo
+     * Purpose: Accepts a string from the user and outputs a string with the characters in reverse order
+     */
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            //turns input into a character array to reverse it and then turns it back into a string
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed = new string(charArray);
+
+            Console.WriteLine("Reversed string: " + reversed);
+        }
+    }
+}

c16067d2d2f5b823c8d0b97f3350a8d2e0d20612
Sebastian Arroyo <sebkiller245@gmail.com>
2023-09-20 23:51:31 -0400

    PE8 question 9

---
+++ PE8-9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_9
+{
+    /* Author: Sebastian Arroyo
+     * Purpose: Accepts a string and adds double quotes around each word.
+     */
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            // Split the input string into words
+            string[] words = input.Split();
+
+            // Add double quotes around each word
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = '"' + words[i] + '"';
+                Console.Write(words[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}

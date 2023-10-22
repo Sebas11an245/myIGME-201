6306c66ca19208b8f8bdd7bbd5a192459ab2ec77
Sebastian Arroyo <sebkiller245@gmail.com>
2023-10-22 18:50:42 -0400

    PE12

---
+++ PE12/PE12_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PE12_3
+{
+    public class MyClass
+    {
+        private string myString;
+        public string MyString
+        {
+            set
+            {
+                this.myString = value;
+            }
+        }
+        public virtual string GetString()
+        {
+            return this.myString;
+        }
+    }
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+            return base.GetString() + " (output from the derived class)";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass myDerivedClass = new MyDerivedClass();
+            myDerivedClass.MyString = "supercalifragilisticexpialidocious";
+            Console.WriteLine(myDerivedClass.GetString());
+        }
+    }
+}

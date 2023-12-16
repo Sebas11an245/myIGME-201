98f133e793b3baffef9285c516c2c7e1940592df
Sebastian Arroyo <sebkiller245@gmail.com>
2023-12-16 02:59:17 -0500

    Q1

---
+++ FinalExam/Question 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_1
+{
+    //Author: Sebastian Arroyo
+    //Purpose:Stack without Stack
+    //Restrictions: None
+    class MyStack
+    {
+        private List<int> stackList = new List<int>();
+
+        //Purpose: Push
+        //Restrictions:None
+        public void Push(int n)
+        {
+            stackList.Add(n);
+        }
+        
+        //Purpose: Pop
+        //Restrictions: Can't pop when stack is empty
+        public int Pop()
+        {
+            if (stackList.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty. Cannot Pop.");
+            }
+
+            int poppedValue = stackList[stackList.Count - 1];
+            stackList.RemoveAt(stackList.Count - 1);
+            return poppedValue;
+        }
+
+        //Purpose: Peek
+        //Restrictions: Can't peek when stack is empty
+        public int Peek()
+        {
+            if (stackList.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty. Cannot Peek.");
+            }
+
+            return stackList[stackList.Count - 1];
+        }
+    }
+}
